c028ccf3fbe25b938d0dcc6fb972178c4532834c
Yannick Chevalier <yannick.chevalier@irit.fr>
2018-09-11 15:28:00 +0200

    enonces nettoyes

--- texmf/tex/latex/Cours/auto/testea.el
+++
@@ -1,9 +0,0 @@
-(TeX-add-style-hook "testea"
- (lambda ()
-    (TeX-add-symbols
-     "useformation")
-    (TeX-run-style-hooks
-     "latex2e"
-     "art10"
-     "article")))
-
--- texmf/tex/latex/Cours/auto/testyearcomputation.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook "testyearcomputation"
- (lambda ()
-    (TeX-run-style-hooks
-     "yearcomputation"
-     "latex2e"
-     "art10"
-     "article")))
-
